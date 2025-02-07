74a2c29118bbf7cb65dc26a7c7104a7339dfb696
Sonia-trisher <soniatrisher86@gmail.com>
2025-02-07 07:21:39 -0800

    revision on javascript

---
+++ app.js
@@ -0,0 +1,51 @@
+//kkkkkk
+//  log = console.log();
+//  clg = console.log(object);
+// console.log();
+// console.log('object');
+// declared valiable
+// let name ;
+// assign value
+let banana;
+banana = "banana"
+console.log(banana);
+const youtubeChannel ="mrbeast";
+// youtubeChannel = "huxn webdev";
+console.log(youtubeChannel);
+
+// create is the same to declare and to store is to initialized 
+// primitive types
+// 1.number
+// let num =2;
+// // basic math
+// console.log(2+2) ;
+// console.log(2-2) ;
+// console.log(2*2) ;
+// console.log(2/2) ;
+// console.log(2%2) ;modules
+// console.log(2**2) ;powe
+
+
+let counter = 10;
+// counter--;increement
+counter--;
+console.log(counter);
+
+let firstNum = 5;
+let secondNum= 6;
+add = firstNum+secondNum;
+subtract= firstNum-secondNum;
+multiply= firstNum*secondNum;
+divided= firstNum-secondNum;
+mod= firstNum%secondNum;
+power= firstNum**secondNum;
+
+console.log(firstNum+secondNum);
+console.log(firstNum-secondNum);
+console.log(firstNum*secondNum);
+console.log(firstNum/secondNum);
+console.log(firstNum%secondNum);
+console.log(firstNum**secondNum);
+// check like log same how
+
+ 
