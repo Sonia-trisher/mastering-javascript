ab1234b43fa46694afdb3ced6d53e13e7e4b046e
Sonia-trisher <soniatrisher86@gmail.com>
2025-02-10 07:27:51 -0800

    practice.2

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//kkkkkk
+//valiable
 //  log = console.log();
 //  clg = console.log(object);
 // console.log();
@@ -47,5 +47,146 @@ console.log(firstNum/secondNum);
 console.log(firstNum%secondNum);
 console.log(firstNum**secondNum);
 // check like log same how
+// bollean
+ let notDefined= null;
+ console.log(notDefined);
+
+//  let isIsSoft= true;
+//  let isIsHard= false;
+//  let favNumb =7;
+//  console.log(isIsSoft );
+ 
+//  console.log(favNumb+undefined )
+
+// //  true/false(relation operator)
+// // console.log(10>10); //false
+// // console.log(10<10);//false
+// // console.log(10<=10);//true
+// // console.log(10>=10);//true
+
+// //equality operator
+// //=== strict equality
+// //==lose equality
+// console.log(10===10);//true
+// console.log(10==10);//false
+// console.log(10==="10");//false
+// console.log(10=="10");//true it doesn't matter whether these string or not
+
+
+
+let firstfavNumb= 2;
+let secondfavNumb= 6;
+// console.log(firstfavNumb>secondfavNumb);
+// console.log(firstfavNumb<secondfavNumb);
+// console.log(firstfavNumb>=secondfavNumb);
+// console.log(firstfavNumb<=secondfavNumb);
+// console.log(firstfavNumb===secondfavNumb);
+// console.log(firstfavNumb==secondfavNumb);
+// console.log(firstfavNumb===!secondfavNumb);
+// console.log(firstfavNumb==!secondfavNumb);
+
+// //string
+// let firstName = "sonia";
+// let secondName = 'leys';
+// let fullName = firstName+secondName;
+// console.log(firstName);
+// console.log(secondName);
+// console.log (fullName);
+
+//.concatenation
+// let firstName = "       sonia    ";
+// let secondName = 'leys';
+// console.log(firstName);
+
+// // let fullName= firstName.concat(secondName)
+// // console.log(fullName);
+
+// // .append
+// // firstName += "everything"
+// // console.log(firstName); 
+ 
+// // .length
+// console.log(firstName.length);
+// //.case
+// // console.log(firstName.toLowerCase());
+// // console.log(firstName.toUpperCase());
+// //.slice
+// console.log( firstName.slice(0,3));
+// //.includes
+// console.log(firstName.includes("x"));
+// //.TRIM
+// console.log(firstName.trim());
+
+// //butics
+let desc = `this everything  
+text
+`;
+ console.log(desc);
+
+
+let firstName = "keza";
+let secondName = "Sonia";
+// fullName = `${firstName} ${secondName} everything`;
+// console.log(fullName);
+
+// exercises
+let facActorFirstName = "keza  ";
+let facActorSecondName = "sonia ";
+let fullName = facActorFirstName.concat(facActorSecondName);
+let upperCase= fullName.toUpperCase();
+let message = `my fovorite actor is ${upperCase},`;
+message += "his best  tv show is silicon valley";
+console.log(message);
+
+// let money = "50";
+// console.log(money); 
+// // convert string to number
+// // money = parseInt(money)
+// // money = +money;
+// money = Number(money);
+// console.log(typeof money);
+
+let money = 50;
+console.log(money);
+// convert number to String
+// money = money.toString();
+money = String(money);
+
+console.log(typeof money);
+
+
+// if (condition){...}
+// else if()
+// else
+
+let a = 9;
+let b = 20; 
+
+if (a > b) {
+    console.log("a is greater then b");
+
+}else if(a<b){
+    console.log("a is  less then  b ");
+       
+}else{
+    console.log("a is equal to   b");
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
  
